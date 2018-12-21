11e273333f29803fed20e97471a667f513653a06
Peehsa <44491641+Peehsa@users.noreply.github.com>
2018-12-21 10:47:00 +0800

    Update 09考试题.py

--- 168206133/09考试题.py
+++ 168206133/09考试题.py
@@ -1,7 +1,13 @@
-import os
-thieves=['a','b','c','d']
+thieves=['A','B','C','D']
 for i in range(len(thieves)):
     thief=thieves[i]
-    if ((thief!='a')+(thief=='d')+(thief=='b')+(thief!='d')==1):
+    if ((thief!='A')+(thief=='D')+(thief=='B')+(thief!='D')==1):
         print('偷走钻石的是',thieves[i])
-os.system('pause')
+        if (thief!='A')==1:
+            print('说真话的是A')
+        if (thief=='D')==1:
+            print('说真话的是B')
+        if (thief=='B')==1:
+            print('说真话的是C')
+        if (thief!='D')==1:
+            print('说真话的是D')
